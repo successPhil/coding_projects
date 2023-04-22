f276af9243388dc4888b2f975966becadd78c6a8
successPhil <129565748+successPhil@users.noreply.github.com>
2023-04-22 10:23:30 -0400

    updated line through, remove, clear, added minimal styling

--- react_tutorial/react_todo/todo_list/src/App.js
+++ react_tutorial/react_todo/todo_list/src/App.js
@@ -1,53 +1,78 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [taskList, setTaskList] = useState([]);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    const taskInput = document.getElementById('task-input');
+    const newTask = taskInput.value.trim();
+    if (newTask !== '') {
+      setTaskList([...taskList, { task: newTask, completed: false }]);
+      taskInput.value = '';
+    }
   };
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    if (!inputValue) return;
-    const newTask = {
-      text: inputValue,
-      completed: false
-    };
-    setTasks([...tasks, newTask])
-    setInputValue('');
+  const handleDeleteTask = (taskIndex) => {
+    const newTaskList = [...taskList];
+    newTaskList.splice(taskIndex, 1);
+    setTaskList(newTaskList);
   };
 
-  const handleTaskClick = (index) => {
-    const newTasks = [...tasks];
-    newTasks[index].completed = !newTasks[index].completed;
-    setTasks(newTasks);
+  const handleClearList = () => {
+    setTaskList([]);
   };
 
-  const handleTaskRemove = (index) => {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
+  const handleTaskCompletion = (taskIndex) => {
+    const newTaskList = [...taskList];
+    newTaskList[taskIndex].completed = !newTaskList[taskIndex].completed;
+    setTaskList(newTaskList);
   };
 
   return (
-    <div className="container">
-      <h1 className="title">Tasks</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} placeholder="Add task..." className="input-task" />
-        <button type="submit" className="button-add">Add</button>
-      </form>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index} className="task-item" onClick={() => handleTaskClick(index)}>
-            <input type="checkbox" checked={task.completed} className="task-checkbox" />
-            <span className="task-text">{task.text}</span>
-          </li>
-        ))}
-      </ul>
-      <button className="button-clear" onClick={() => setTasks([])}>Clear</button>
+    <div className="body">
+      <div className="container">
+        <h1 className="title">To-Do List</h1>
+        <form onSubmit={handleAddTask}>
+          <input
+            type="text"
+            id="task-input"
+            className="input-task"
+            placeholder="Add a task..."
+            required
+          />
+          <button type="submit" className="button-add">
+            Add
+          </button>
+        </form>
+        <ul className="list">
+          {taskList.map((taskObj, taskIndex) => (
+            <li className="task-item" key={taskIndex}>
+              <input
+                type="checkbox"
+                className="task-checkbox"
+                checked={taskObj.completed}
+                onChange={() => handleTaskCompletion(taskIndex)}
+              />
+              <p
+                className={`task-text ${taskObj.completed ? 'completed' : ''}`}
+              >
+                {taskObj.task}
+              </p>
+              <button
+                className="task-remove"
+                onClick={() => handleDeleteTask(taskIndex)}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button className="button-clear" onClick={handleClearList}>
+          Clear List
+        </button>
+      </div>
     </div>
   );
 }
